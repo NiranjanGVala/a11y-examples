fca5d784425f809fec25bd23d7f7b61a5f43d4d8
Niranjan <niranjan.v.94@gmail.com>
2022-02-26 11:09:29 +0530

    Improved the keyboard support.

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const ariaButton = document.getElementById("aria-button")
 ariaButton.addEventListener("click", () => alert("Hi! Greetings from the accessible aria button!"))
+ariaButton.addEventListener("keydown", e => {
+    if (e.keyCode === 13) {
+        alert("Hi! Greetings from the accessible aria button!")
+    }
+})
